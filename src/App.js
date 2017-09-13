7c97f4ee6e91150cc6dcab32ebe5460e697d2552
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-09-13 19:10:25 +0300

    media queries added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
+var grid;
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: this.nextMove()
+      hidth:20,
+      maxTurn:40,
+      maxRandLength:8
     };
   }
   fullArray (num, hidth) {
@@ -20,15 +21,15 @@ class App extends Component {
     return array;
   }
   nextMove () {
-    var hidth = 20;
-    var maxTurn = 40; // the number of turns the tunnel has
-    var maxLength = 8; // maximum number of each turn can have
-    var fulArr = this.fullArray(1, hidth); // save a full array;
-    var curRow = Math.floor(Math.random() * hidth);// pick a random row
-    var curCol = Math.floor(Math.random() * hidth);// pick a random column
-    var calcObject = [[-1, 0],[1, 0],[0, -1],[0, 1]];// turns allowed are horizontal right/left and vertical right/left
-    var lastTurn = [3, 3];// a variable to save the last turn it could have any non -1,0,1 value
-    while (maxTurn) {// while number of allowed turns are not 0 do the following
+    let hidth = this.state.hidth,
+     maxTurn = this.state.maxTurn, // the number of turns the tunnel has
+     maxRandLength = this.state.maxRandLength, // maximum number of each turn can have
+     fulArr = this.fullArray(1, hidth), // save a full array;
+     curRow = Math.floor(Math.random() * hidth),// pick a random row
+     curCol = Math.floor(Math.random() * hidth),// pick a random column
+     calcObject = [[-1, 0],[1, 0],[0, -1],[0, 1]],// turns allowed are horizontal right/left and vertical right/left
+     lastTurn = [3, 3];// a variable to save the last turn it could have any non -1,0,1 value
+     while (maxTurn) {// while number of allowed turns are not 0 do the following
       var randTurn;// initiate variable for the turn
       do {// do the following and check to see if passes the condition
         randTurn = calcObject[Math.floor(Math.random() * calcObject.length)];//randomly choose a turn
@@ -36,8 +37,8 @@ class App extends Component {
         //if the turn is similar to the previously selected turn or is reverse of the previous turn select another turn
       } while (((randTurn[0] === -1 * lastTurn[0]) && (randTurn[1] === -1 * lastTurn[1])) ||
                ((randTurn[0] === lastTurn[0]) && (randTurn[1] === lastTurn[1])));
-      var leng = Math.ceil(Math.random() * maxLength);//randomly choose a length form the maximum allowed length
-      for (var i = 0; i < leng; i++) {//loop over the length
+      var randLeng = Math.ceil(Math.random() * maxRandLength);//randomly choose a length form the maximum allowed length
+      for (var i = 0; i < randLeng; i++) {//loop over the length
           fulArr[curRow][curCol] = 0;//set the value of the item to 0
         //break the loop if it is going out of the map
           if(((curRow === 0) && (randTurn[0] === -1)) ||
@@ -57,10 +58,11 @@ class App extends Component {
     return fulArr;// finally retun the array to be drawn
   };
   render() {
+    grid = this.nextMove();
     return (
   <table className="grid">
   <thead>
-    {this.state.grid.map((obj, row) =>
+    {grid.map((obj, row) =>
         <tr key={row}>{obj.map((obj2, col) =><td className={obj2 === 1 ? 'wall' : 'tunnel'} key={col}></td>)}</tr>
     )}
     </thead>
