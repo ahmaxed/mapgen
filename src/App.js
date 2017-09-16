985570bbf67fd70a69ce732b4ab3219294b37c7f
moT01 <TMondloch01@gmail.com>
2017-09-16 14:20:06 -0500

    fix max edge length

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       grid:[]
     };
     this.onClick = this.onClick.bind(this);
-    this.onChange = this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this);
   }
   fullArray (num, dimentions) {
     var array = [];
@@ -29,7 +29,8 @@ class App extends Component {
 onSubmit(e){
   e.preventDefault();
 }
-  nextMove () {
+
+nextMove () {
 
     let dimentions = this.state.dimentions,
      maxTurn = this.state.maxTurn, // the number of turns the tunnel has
@@ -37,35 +38,37 @@ onSubmit(e){
      fulArr = this.fullArray(1, dimentions), // save a full array;
      curRow = Math.floor(Math.random() * dimentions),// pick a random row
      curCol = Math.floor(Math.random() * dimentions),// pick a random column
-     allowedTurns = [[-1, 0],[1, 0],[0, -1],[0, 1]],// turns allowed are horizontal right/left and vertical right/left
+     allowedTurns = [[-1, 0],[1, 0],[0, -1],[0, 1]],// turns allowed are horizontal right/left and vertical right/left //left,right,up,down
      lastTurn = [3, 3];// a variable to save the last turn it could have any non -1,0,1 value
      while (maxTurn) {// while number of allowed turns are not 0 do the following
       var randTurn;// initiate variable for the turn
+      
       do {// do the following and check to see if passes the condition
         randTurn = allowedTurns[Math.floor(Math.random() * allowedTurns.length)];//randomly choose a turn
         //if the player is at the edge of the map and the random turn wants to push it off map select another turn
         //if the turn is similar to the previously selected turn or is reverse of the previous turn select another turn
-      } while (((randTurn[0] === -1 * lastTurn[0]) && (randTurn[1] === -1 * lastTurn[1])) ||
+      } while (((randTurn[0] === -1 * lastTurn[0]) && (randTurn[1] === -1 * lastTurn[1])) || //lT = [1,0] //rT = [0,-1]
                ((randTurn[0] === lastTurn[0]) && (randTurn[1] === lastTurn[1])));
       var randLeng = Math.ceil(Math.random() * maxLeng);//randomly choose a length form the maximum allowed length
+ 
       for (var i = 0; i < randLeng; i++) {//loop over the length
 
         //break the loop if it is going out of the map
-          if(((curRow === 0) && (randTurn[0] === -1)) ||
-             ((curCol === 0) && (randTurn[1] === -1)) ||
-             ((curRow === dimentions - 1) && (randTurn[0] === 1)) ||
-             ((curCol === dimentions - 1) && (randTurn[1] === 1))){
-               debugger;
-            break
-          }else{
-            //set the value of the item to 0
-            fulArr[curRow][curCol] = 0;
-            //otherwise incriment the row and col according to the turn
-            curRow = curRow + randTurn[0];
-            curCol = curCol + randTurn[1];
-          }
-      }
-      lastTurn = randTurn;//set last turn to the value of the current turn
+	       if(((curRow === 0) && (randTurn[0] === -1)) ||
+	          ((curCol === 0) && (randTurn[1] === -1)) ||
+	          ((curRow === dimentions - 1) && (randTurn[0] === 1)) ||
+	          ((curCol === dimentions - 1) && (randTurn[1] === 1))){
+	            //debugger;
+	         	break;
+	       }else{
+	         //set the value of the item to 0
+	         fulArr[curRow][curCol] = 0;
+	         //otherwise incriment the row and col according to the turn
+	         curRow = curRow + randTurn[0];
+	         curCol = curCol + randTurn[1];
+	       }
+	   	lastTurn = randTurn;//set last turn to the value of the current turn
+	   }
       maxTurn--;// decrement the number of turns allowed
     }
     return fulArr;// finally return the array to be drawn
